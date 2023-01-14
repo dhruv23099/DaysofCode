760fec8879d3f007534d43513cccb26aa381ace7
dhruv23099 <98419503+dhruv23099@users.noreply.github.com>
2023-01-14 22:34:37 +0530

    Update script.js

--- Day 5/script.js
+++ Day 5/script.js
@@ -13,12 +13,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const TIE = 'TIE';
 
 
-    /*
-        Indexes within the board
-        [0] [1] [2]
-        [3] [4] [5]
-        [6] [7] [8]
-    */
+    
 
     const winningConditions = [
         [0, 1, 2],
